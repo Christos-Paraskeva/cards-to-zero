3e8b8e240ed507a7447622a38d080d7949e57ad8
Christos Paraskeva <chris.paraskeva10@gmail.com>
2017-11-08 15:12:27 +0000

    All tests pass for player class

--- src/models/player.ts
+++ src/models/player.ts
@@ -5,6 +5,22 @@ export class Player {
 
     public constructor(id: number, name: string) { this.id = id;
                                                    this.name = name;
-                                                   this.cardsHeld = []
+                                                   this.cardsHeld = [];
                                                  }
+
+    public getId() {
+      return this.id;
+    }
+
+    public getName() {
+      return this.name;
+    }
+
+    public showCards() {
+      return this.cardsHeld;
+    }
+
+    public createPlayer(id, name) {
+      return new Player(id, name);
+    }
 }
--- test/models/player.spec.ts
+++ test/models/player.spec.ts
@@ -24,7 +24,29 @@ describe("Player", () => {
       let player = new Player(1, 'Test Name');
       expect(player.cardsHeld).to.eql([]);
     });
-
   });
 
+  describe('Can check for', () => {
+
+    it("current player's name", () => {
+      let player = new Player(1, 'Test Name');
+      expect(player.getName()).to.equal('Test Name');
+    });
+
+    it("current player's id", () => {
+      let player = new Player(1, 'Test Name');
+      expect(player.getId()).to.equal(1);
+    });
+
+    it("current player's held cards", () => {
+      let player = new Player(1, 'Test Name');
+      expect(player.showCards()).to.eql([]);
+    });
+  });
+
+  it("can create a new player", () => {
+    let player = new Player(1, 'Test Name');
+    let playerTwo = player.createPlayer(2, 'Player Two');
+    expect(playerTwo).to.be.an.instanceof(Player);
+  });
 });
