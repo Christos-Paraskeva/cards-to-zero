699408af0552052a27c73c9831f1df6e7a27497d
Christos Paraskeva <chris.paraskeva10@gmail.com>
2017-11-08 14:59:54 +0000

    Further tests for player pass

--- src/models/player.ts
+++ src/models/player.ts
@@ -1,4 +1,10 @@
 export class Player {
     public id: number;
-    public constructor(id: number) { this.id = id; }
+    public name: string;
+    public cardsHeld: object[];
+
+    public constructor(id: number, name: string) { this.id = id;
+                                                   this.name = name;
+                                                   this.cardsHeld = []
+                                                 }
 }
--- test/models/player.spec.ts
+++ test/models/player.spec.ts
@@ -4,15 +4,25 @@ import { Player } from '../../src/models/player';
 describe("Player", () => {
 
   it("exists", () => {
-    let player = new Player(1);
+    let player = new Player(1, 'Test Name');
     expect(player).not.to.be.undefined;
     expect(player).to.be.an.instanceof(Player);
   });
 
   describe("Is initialized with", () => {
-    it("an ID", () => {
-      let player = new Player(1);
+    it("an id", () => {
+      let player = new Player(1, 'Test Name');
       expect(player.id).to.equal(1);
+    });
+
+    it("a name", () => {
+      let player = new Player(1, 'Test Name');
+      expect(player.name).to.equal('Test Name');
+    });
+
+    it("an empty array for currently held cards", () => {
+      let player = new Player(1, 'Test Name');
+      expect(player.cardsHeld).to.eql([]);
     });
 
   });
